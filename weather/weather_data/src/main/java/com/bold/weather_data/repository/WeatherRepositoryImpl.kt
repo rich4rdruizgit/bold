121311e4b61984c6ae2d390d3340eb2753909989
rich4rdruizgit <rich4rdruiz@gmail.com>
2022-04-09 13:58:04 -0500

    weather_data: implementando el repositorio

--- weather/weather_data/src/main/java/com/bold/weather_data/repository/WeatherRepositoryImpl.kt
+++ weather/weather_data/src/main/java/com/bold/weather_data/repository/WeatherRepositoryImpl.kt
@@ -6,6 +6,16 @@ import com.bold.weather_domain.repository.WeatherRepository
 
 class WeatherRepositoryImpl(private val api: MetaWeatherApi) : WeatherRepository {
     override suspend fun searchLocation(query: String): Result<List<LocationItem>> {
-        TODO("Not yet implemented")
+        return try {
+            val searchDto = api.searchLocation(query)
+            Result.success(
+                searchDto.map { LocationItem(it.latLong, it.locationType, it.title, it.woeid) }
+            )
+        } catch (
+            e: Exception
+        ) {
+            e.printStackTrace()
+            Result.failure(e)
+        }
     }
 }
